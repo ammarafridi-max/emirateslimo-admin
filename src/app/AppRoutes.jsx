4c52aa7e1bb6fb183526c356a5dce7f9eda14b8a
Ammar Afridi <ammar.afridi95@gmail.com>
2025-11-04 10:34:54 +0400

    New routes for currencies, bookings, users

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -50,39 +50,39 @@ export default function AppRoutes() {
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route element={<PrivateRoutes />}> */}
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="account" element={<MyAccount />} />
-          <Route path="zones" element={<Zones />} />
-          <Route path="zones/create" element={<CreateZone />} />
-          <Route path="zones/:id" element={<UpdateZone />} />
-          <Route path="vehicles" element={<Vehicles />} />
-          <Route path="vehicles/create" element={<CreateVehicle />} />
-          <Route path="vehicles/:id" element={<UpdateVehicle />} />
-          <Route path="pricing" element={<PricingList />} />
-          <Route path="pricing/create" element={<CreatePricing />} />
-          <Route path="pricing/:id" element={<UpdatePricing />} />
-          <Route path="availability-rules" element={<AvailabilityRules />} />
-          <Route
-            path="availability-rules/create"
-            element={<CreateAvailabilityRule />}
-          />
-          <Route
-            path="availability-rules/:id"
-            element={<UpdateAvailabilityRule />}
-          />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="bookings/:id" element={<UpdateBooking />} />
-          <Route path="currencies" element={<Currencies />} />
-          <Route path="currencies/create" element={<CreateCurrency />} />
-          <Route path="currencies/:id" element={<UpdateCurrency />} />
-          <Route path="users" element={<Users />} />
-          <Route path="users/create" element={<CreateUser />} />
-          <Route path="users/:id" element={<UpdateUser />} />
+        <Route element={<PrivateRoutes />}>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<MyAccount />} />
+            <Route path="zones" element={<Zones />} />
+            <Route path="zones/create" element={<CreateZone />} />
+            <Route path="zones/:id" element={<UpdateZone />} />
+            <Route path="vehicles" element={<Vehicles />} />
+            <Route path="vehicles/create" element={<CreateVehicle />} />
+            <Route path="vehicles/:id" element={<UpdateVehicle />} />
+            <Route path="pricing" element={<PricingList />} />
+            <Route path="pricing/create" element={<CreatePricing />} />
+            <Route path="pricing/:id" element={<UpdatePricing />} />
+            <Route path="availability-rules" element={<AvailabilityRules />} />
+            <Route
+              path="availability-rules/create"
+              element={<CreateAvailabilityRule />}
+            />
+            <Route
+              path="availability-rules/:id"
+              element={<UpdateAvailabilityRule />}
+            />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:id" element={<UpdateBooking />} />
+            <Route path="currencies" element={<Currencies />} />
+            <Route path="currencies/create" element={<CreateCurrency />} />
+            <Route path="currencies/:id" element={<UpdateCurrency />} />
+            <Route path="users" element={<Users />} />
+            <Route path="users/create" element={<CreateUser />} />
+            <Route path="users/:id" element={<UpdateUser />} />
+          </Route>
         </Route>
-        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
