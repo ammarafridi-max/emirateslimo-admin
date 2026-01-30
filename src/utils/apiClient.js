1470ed8eaab4f84d352cf9cb72db2c9716e648f5
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-30 10:48:25 +0400

    Return null on 204 delete requests

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -12,6 +12,7 @@ async function checkError(res) {
 }
 
 async function returnData(res) {
+  if (res.status === 204) return null;
   const json = await res.json();
   return json.data || null;
 }
@@ -20,7 +21,6 @@ export async function apiFetch(path, options = {}) {
   const res = await fetch(`${BACKEND}${path}`, {
     ...options,
     headers: {
-      'Content-Type': 'application/json',
       ...(options.headers || {}),
     },
     credentials: 'include',
@@ -33,7 +33,7 @@ export async function apiFetch(path, options = {}) {
 export async function apiUpload(path, formData, method = 'POST') {
   const res = await fetch(`${BACKEND}${path}`, {
     method,
-    credentials: 'include', // 🔥 MUST HAVE
+    credentials: 'include',
     body: formData,
   });
 
